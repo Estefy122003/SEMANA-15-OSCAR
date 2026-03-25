e81c1b8ddaa56e2570d252fee5353e2e3b8611c0
Estefy122003 <en.narvaezz@uea.edu.ec>
2026-03-25 11:03:30 -0500

    Proyecto Lista de Tareas con Tkinter

---
+++ ui/app_tkinter.py
@@ -0,0 +1,67 @@
+import tkinter as tk
+from tkinter import messagebox
+
+class AppTkinter:
+    def __init__(self, root, servicio):
+        self.root = root
+        self.servicio = servicio
+
+        self.root.title("Lista de Tareas")
+
+        # Entrada
+        self.entry = tk.Entry(root, width=40)
+        self.entry.pack(pady=10)
+
+        # Evento teclado (ENTER)
+        self.entry.bind("<Return>", self.agregar_tarea_evento)
+
+        # Lista
+        self.lista = tk.Listbox(root, width=50)
+        self.lista.pack()
+
+        # Evento doble clic (extra)
+        self.lista.bind("<Double-1>", self.marcar_completada_evento)
+
+        # Botones
+        tk.Button(root, text="Añadir Tarea", command=self.agregar_tarea).pack(pady=5)
+        tk.Button(root, text="Marcar Completada", command=self.marcar_completada).pack(pady=5)
+        tk.Button(root, text="Eliminar", command=self.eliminar_tarea).pack(pady=5)
+
+    def actualizar_lista(self):
+        self.lista.delete(0, tk.END)
+        for tarea in self.servicio.obtener_tareas():
+            texto = tarea.descripcion
+            if tarea.completada:
+                texto = "[Hecho] " + texto
+            self.lista.insert(tk.END, f"{tarea.id}. {texto}")
+
+    def agregar_tarea(self):
+        texto = self.entry.get()
+        if texto:
+            self.servicio.agregar_tarea(texto)
+            self.entry.delete(0, tk.END)
+            self.actualizar_lista()
+        else:
+            messagebox.showwarning("Error", "Escribe una tarea")
+
+    def agregar_tarea_evento(self, event):
+        self.agregar_tarea()
+
+    def marcar_completada(self):
+        seleccion = self.lista.curselection()
+        if seleccion:
+            index = seleccion[0]
+            tarea = self.servicio.obtener_tareas()[index]
+            self.servicio.completar_tarea(tarea.id)
+            self.actualizar_lista()
+
+    def marcar_completada_evento(self, event):
+        self.marcar_completada()
+
+    def eliminar_tarea(self):
+        seleccion = self.lista.curselection()
+        if seleccion:
+            index = seleccion[0]
+            tarea = self.servicio.obtener_tareas()[index]
+            self.servicio.eliminar_tarea(tarea.id)
+            self.actualizar_lista()
